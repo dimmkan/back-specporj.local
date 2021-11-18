ef8b0984e7b8974ea021e497e84ffeffbb3f3c5c
dimmkan <dimmkan@mail.ru>
2021-11-18 08:48:55 +0300

    User controller

---
+++ controllers/user.js
@@ -0,0 +1,83 @@
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+class userController {
+
+    async getAll(req, res) {
+        try {
+            const users = await User.findAll()
+            res.status(200).json({users})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            const user = await User.findByPk(+req.params.id)
+            res.status(200).json({user})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+
+    async createUser(req, res) {
+        try {
+            const hashPassword = await bcrypt.hash(req.body.password, 10)
+            const user = await User.create({
+                email: req.body.email,
+                password: hashPassword,
+                role: req.body.role ?? 'user',
+                filialID: isNaN(+req.body.filialID) ? null : +req.body.filialID
+            })
+            res.status(201).json({user})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+
+    async updateUser(req, res) {
+        try {
+            let user = await User.findByPk(+req.params.id)
+            for (const bodyKey in req.body) {
+                if (bodyKey === 'password') {
+                    user[bodyKey] = await bcrypt.hash(req.body[bodyKey], 10)
+                } else {
+                    user[bodyKey] = req.body[bodyKey]
+                }
+            }
+            user.save()
+            res.status(200).json({user})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findByPk(+req.params.id)
+            await user.destroy()
+            res.status(204).json({})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+
+}
+
+module.exports = new userController()
--- routes/user.js
+++ routes/user.js
@@ -1,83 +1,31 @@
 const router = require('express').Router()
-const User = require('../models/user')
-const bcrypt = require('bcryptjs')
+const userController = require('../controllers/user')
 const passport = require('passport')
 
 
 //Получение списка пользователей
 router.get('/', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try{
-        const users = await User.findAll()
-        res.status(200).json({users})
-    }catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Internal server error'
-        })
-    }
+    await userController.getAll(req, res)
 })
+
 //Получение данных пользователя
 router.get('/:id', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try{
-        const user = await User.findByPk(+req.params.id)
-        res.status(200).json({user})
-    }catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Internal server error'
-        })
-    }
+    await userController.getById(req, res)
 })
+
 //Добавление пользователя
 router.post('/', passport.authenticate('jwt', {session: false}), passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try{
-        const hashPassword = await bcrypt.hash(req.body.password, 10)
-        const user = await User.create({
-            email: req.body.email,
-            password: hashPassword,
-            role: req.body.role ?? 'user',
-            filialID: isNaN(+req.body.filialID) ? null : +req.body.filialID
-        })
-        res.status(201).json({user})
-    }catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Internal server error'
-        })
-    }
+    await userController.createUser(req, res)
 })
-//Изменение филиала
+
+//Изменение пользователя
 router.put('/:id', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try{
-        let user = await User.findByPk(+req.params.id)
-        for(const bodyKey in req.body){
-            if(bodyKey === 'password'){
-                user[bodyKey] = await bcrypt.hash(req.body[bodyKey], 10)
-            }else{
-                user[bodyKey] = req.body[bodyKey]
-            }
-        }
-        user.save()
-        res.status(200).json({user})
-    }catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Internal server error'
-        })
-    }
+    await userController.updateUser(req, res)
 })
-//Удаление филиала
+
+//Удаление пользователя
 router.delete('/:id', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try{
-        const user = await User.findByPk(+req.params.id)
-        await user.destroy()
-        res.status(204).json({})
-    }catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Internal server error'
-        })
-    }
+    await userController.deleteUser(req, res)
 })
 
 module.exports = router
